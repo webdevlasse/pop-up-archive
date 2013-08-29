309bc922d44fbfc3a9c5881ea4f0af2bbec41f94
baileyspace <bailey.r.smith@gmail.com>
2013-08-29 16:15:17 -0700

    API endpoint to angular

--- app/assets/javascripts/dashboard/controllers.js
+++ app/assets/javascripts/dashboard/controllers.js
@@ -1,5 +1,6 @@
 angular.module('Directory.dashboard.controllers', ['Directory.loader', 'Directory.user'])
-.controller('DashboardCtrl', [ '$scope', 'Item', 'Loader', 'Me', function ItemsCtrl($scope, Item, Loader, Me) {
+.controller('DashboardCtrl', [ '$scope', 'Item', 'Loader', 'Me', 'Dashboard', function ItemsCtrl($scope, Item, Loader, Me, Dashboard) {
+  new Dashboard();
   Me.authenticated(function (data) {
   });
 }])
