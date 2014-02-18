3bb6bdf0f30fb0a10a17084764eede7ef094334b
Lasse Christiansen <webdevlasse@gmail.com>
2014-02-18 15:04:58 -0800

    changes to views in admin/accounts

--- app/controllers/admin/accounts_controller.rb
+++ app/controllers/admin/accounts_controller.rb
@@ -5,7 +5,10 @@ class Admin::AccountsController < Admin::BaseController
   end
 
   def total_usage
-    @accounts = @accounts
+    @accounts = @accounts.paginate(:page => params[:page],
+                                   :per_page => 5,
+                                   :order => ('used_metered_storage_cache DESC,
+                                                               created_at DESC'))
   end  
 
 end
