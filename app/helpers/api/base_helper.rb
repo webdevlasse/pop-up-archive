26594118713b9e44a648d6d14cfaf178a0732743
Lasse Christiansen <webdevlasse@gmail.com>
2014-02-18 12:58:44 -0800

    total_time_in_hours

--- app/helpers/api/base_helper.rb
+++ app/helpers/api/base_helper.rb
@@ -17,10 +17,17 @@ module Api::BaseHelper
     time_definition(total_time)
   end
 
+  def total_time_in_hours
+    u = User.all
+    total_time = u.map {|user| [user.used_unmetered_storage, user.used_metered_storage] }.flatten.sum
+    hours = (total_time / 3600).to_s + "hrs"
+    return hours
+  end        
+
   def time_definition(total_time)
     mm, ss = total_time.divmod(60)            
     hh, mm = mm.divmod(60)           
-    dd, hh = hh.divmod(24)          
+    dd, hh = hh.divmod(24)         
     time = "%dd: %dh: %dm: %ds" % [dd, hh, mm, ss]
     return time
   end  	
